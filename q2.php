dd4f660bb6e6cab40ceef1c403d3009bed1dc776
Aluno <gigiovanagouvea@gmail.com>
2018-08-20 09:06:53 -0300

    questao dois

--- q2.php
+++ q2.php
@@ -6,6 +6,22 @@
 
         <link rel="stylesheet" type= "text/css" href="css/style.css"/>
 
+    <script>
+        function allowDrop(ev) {
+            ev.preventDefault();
+        }
+
+        function drag(ev) {
+            ev.dataTransfer.setData("text", ev.target.id);
+        }
+
+        function drop(ev) {
+            ev.preventDefault();
+            var data = ev.dataTransfer.getData("text");
+            ev.target.appendChild(document.getElementById(data));
+        }
+    </script>
+
   </head>
   <body>
     <div id="menu-topo">
@@ -24,17 +40,24 @@
           <p>Separe as palavras de acordo com as suas classes gramaticais:</p>
         </div>
 
-        <div class="montante">
-          <span>Andar</span>
-          <span>Comer</span>
-          <span>Bonita</span>
+        <div class="montante" ondrop="drop(event)" ondragover="allowDrop(event)">
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Sorrir </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Inteligente </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Partir </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Estudar </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Brincar </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Estudioso </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Esforçado </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Engraçado</span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Feliz </span>
+          <span  draggable="true" ondragstart="drag(event)" id="drag1" width="88" height="31"> Comer </span>
         </div>
 
 
 
         <div class="container">
 
-          <div id="containerVerbos">
+          <div id="containerVerbos" ondrop="drop(event)" ondragover="allowDrop(event)">
             <div class= "containerVerbos" id="montanteVerbos" >
               <div id="nomeVerbos" align="center"><b>Verbos</b></div>
             </div>
