21ab8a2872d8356db1374627624593529729d5c7
unknown <gigiovanagouvea@gmail.com>
2018-08-23 14:08:20 -0300

    telas

---
+++ menu.php
@@ -0,0 +1,12 @@
+<?php
+   <div id="menu-topo">
+      <ul>
+        <li> <a href="#"><b>Página Inicial</b></a> </li>
+        <li> <a href="cadastro.html"><b>Cadastro</b></a> </li>
+        <li> <a href="login.html"><b>Login</b></a> </li>
+        <li> <a href="#"><b>Descrição do Jogo</b></a> </li>
+        <li> <a href="#"><b>Como Jogar</b></a> </li>
+        <li> <a href="#"><b>Sobre Nós</b></a> </li>
+      </ul>
+    </div>
+     ?>
